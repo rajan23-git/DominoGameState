1b09bbf192e477fac1051da2dae9c9069edee120
Connor Burk <cburk0709@gmail.com>
2021-10-01 12:31:03 -0700

    Implemented a toString method that prints out the scores for each player. Added score variables for players 3 and 4. Fixed whitespace/misc. formatting issues.

--- app/src/main/java/com/example/dominogamestate/GameState.java
+++ app/src/main/java/com/example/dominogamestate/GameState.java
@@ -3,13 +3,26 @@ package com.example.dominogamestate;
 public class GameState {
     private int playerOneScore;
     private int playerTwoScore;
+    private int playerThreeScore;
+    private int playerFourScore;
+    private int playerCount;
 
     public GameState() {
-playerOneScore=0;
-playerTwoScore=0;
+        playerOneScore = 0;
+        playerTwoScore = 0;
+        playerThreeScore = 0;
+        playerFourScore = 0;
+    }
+    public String toString(){
+        String s = "Player One Score: " + playerOneScore + ".\nPlayer Two Score: " + playerTwoScore + ".";
+        if (playerCount <= 3){
+            s += "\nPlayer Three Score: " + playerThreeScore + ".";
+        }
+        if (playerCount == 4){
+            s += "\nPlayer Four Score: " + playerFourScore + ".";
+        }
 
-
-
+        return s;
     }
 
 
