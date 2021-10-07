a566d5da552cc959103c007089b93351a76294d4
David <led23@up.edu>
2021-10-06 18:02:33 -0700

    MainActivity's onClickListener is functionally complete. Needs work on placing correct moves

--- app/src/main/java/com/example/dominogamestate/MainActivity.java
+++ app/src/main/java/com/example/dominogamestate/MainActivity.java
@@ -43,28 +43,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //and whoever does, they will go first.
             int[] firstMove = firstInstance.firstMove();
 
-            e.setText("Player 3 goes first and places down a domino[6|6]");
+            e.append("Player 3 goes first and places down a domino[6|6]");
             firstInstance.placePiece(firstMove[0],firstMove[1],2,5);
-            e.setText(firstInstance.toString());
+            e.append(firstInstance.toString());
 
-            e.setText("player 2 goes second and places down");
+            e.append("player 2 goes second and places down");
             firstInstance.placePiece(1,0,0,0);
-            e.setText(firstInstance.toString());
+            e.append(firstInstance.toString());
 
-            e.setText("player 1 goes third and does not have valid placePiece");
+            e.append("player 1 goes third and does not have valid placePiece");
             firstInstance.placePiece(0,0,0,0);
-            e.setText(firstInstance.toString());
+            e.append(firstInstance.toString());
 
-            e.setText("player 1 draws dominoes until they can make a valid move");
+            e.append("player 1 draws dominoes until they can make a valid move");
             firstInstance.drawPiece(0);
-            e.setText(firstInstance.toString());
+            e.append(firstInstance.toString());
 
-            e.setText("player 1 goes again and places down");
+            e.append("player 1 goes again and places down");
             firstInstance.placePiece(0,0,0,0);
-            e.setText(firstInstance.toString());
+            e.append(firstInstance.toString());
 
+            //creates a third instance of the game and sets it to secondInstance
+            GameState thirdInstance = new GameState(secondInstance);
 
-
+            //checks if third=second and if our copy constructor works
+            if (thirdInstance.toString().equals(secondInstance.toString()))
+            {
+                e.append(thirdInstance.toString());
+                e.append(secondInstance.toString());
+            }
+            else
+            {
+                e.append("ThirdInstance and SecondInstance do not match.")
+            }
 
 
 
